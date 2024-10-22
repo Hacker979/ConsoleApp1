b621b815cad0f30a5012eb69e8354de09031fdd0
Kab-35-16 <Kab-35-16@KAB-31-11->
2024-10-22 09:54:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine($"c= {c}"); // Fixed the typo from Writeline to WriteLine
+            Console.Read(); // Added missing semicolon
+        }
+    }
+}
